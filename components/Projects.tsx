7e77c78f04ba55b87ac0aa430f4e5b512976866a
Sameer Kamble <sameerkamble1130@gmail.com>
2024-12-13 20:50:31 +0530

    commit check

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard";
 
 export default function Projects() {
     return (
-        <div className = "h-screen mt-12">
+        <div className = " mt-12">
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 <ProjectCard 
                 img="/Images/d-wallet.jpeg"
